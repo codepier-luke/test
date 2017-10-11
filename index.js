dfbd48470a5cef012239d3ae551c8f757fd33b48
Luke Policinski <luke.policinski@egov.com>
2017-10-11 14:00:20 -0400

    new

---
+++ index.js
@@ -0,0 +1,12 @@
+hello ( "world"
+);
+
+[ "lorem", "ipsum", 'dolor', sit("amet"), consectetur[ 'adipiscing' ] + "elit" ].reduce(
+  (first, second) => first + second,
+  "")
+
+const Foo = ({ bar, baz, things }) => {
+  return <div style={{
+color: "papayawhip"}}>
+    <br/>{things.map(thing => reallyLongPleaseDontPutOnOneLine(thing) ? <p>{ok}</p> : <Quax bar={bar} baz={ baz } {...thing}></Quax>)
+  }</div>}
